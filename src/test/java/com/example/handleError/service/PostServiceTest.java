bb023574f8765b1e5f275729760cabe93432b906
weno <logicalstark1105@gmail.com>
2021-07-14 12:34:57 +0900

    test: PostService list 테스트 실패하라

--- src/test/java/com/example/handleError/service/PostServiceTest.java
+++ src/test/java/com/example/handleError/service/PostServiceTest.java
@@ -1,18 +1,29 @@
 package com.example.handleError.service;
 
+import com.example.handleError.entity.Post;
 import com.example.handleError.entity.PostRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.mock;
 
 
+@DisplayName("PostService 테스트")
 class PostServiceTest {
 
     private PostRepository postRepository = mock(PostRepository.class);
     private PostService postService = new PostService(postRepository);
 
-    @Test
-    void testList(){
+    @Nested
+    @DisplayName("list 메소드는")
+    class Describe_list{
+        @Test
+        @DisplayName("모든 Post를 리턴한다.")
+        void It_return_allPost(){
+            Post post = postService.list();
+        }
 
     }
+
 }
