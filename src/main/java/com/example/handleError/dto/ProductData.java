6c7a8d46156e98f81d3f32a4d42c0a5ad61718f0
weno <logicalstark1105@gmail.com>
2021-07-14 13:31:46 +0900

    fix: validation 적용하라

--- src/main/java/com/example/handleError/dto/ProductData.java
+++ src/main/java/com/example/handleError/dto/ProductData.java
@@ -1,7 +1,11 @@
 package com.example.handleError.dto;
 
+import javax.validation.constraints.NotBlank;
+
 public class ProductData {
     private Long id;
+    @NotBlank
     private String title;
+    @NotBlank
     private String content;
 }
