22fb9e3e21984018aafc90d6ed802e4f9971dbb1
weno <logicalstark1105@gmail.com>
2021-07-14 12:05:45 +0900

    feat: ProductController list 메소드 생성하고 테스트 성공하라

--- src/main/java/com/example/handleError/controller/PostController.java
+++ src/main/java/com/example/handleError/controller/PostController.java
@@ -1,7 +1,14 @@
 package com.example.handleError.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/post")
 @RestController
 public class PostController {
+    @GetMapping("list")
+    public void list(){
+
+    }
 }
