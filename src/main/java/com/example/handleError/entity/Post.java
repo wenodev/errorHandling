911b7f6b50db265f82762c2aa0bb977a97f07923
weno <logicalstark1105@gmail.com>
2021-07-14 12:18:35 +0900

    feat: Post, PostRepository 생성하라

---
+++ src/main/java/com/example/handleError/entity/Post.java
@@ -0,0 +1,4 @@
+package com.example.handleError.entity;
+
+public class Post {
+}
--- src/test/java/com/example/handleError/service/PostServiceTest.java
+++ src/test/java/com/example/handleError/service/PostServiceTest.java
@@ -1,7 +1,12 @@
 package com.example.handleError.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 
 class PostServiceTest {
 
+    @Test
+    void testList(){
+
+    }
 }
