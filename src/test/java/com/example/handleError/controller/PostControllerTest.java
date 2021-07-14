20062e0c3856ee06a61bb3294f130385cfe6a497
weno <logicalstark1105@gmail.com>
2021-07-14 11:58:21 +0900

    test: list 테스트 실패하라

--- src/test/java/com/example/handleError/controller/PostControllerTest.java
+++ src/test/java/com/example/handleError/controller/PostControllerTest.java
@@ -1,9 +1,14 @@
 package com.example.handleError.controller;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(PostController.class)
 @DisplayName("PostController 테스트")
@@ -11,5 +16,16 @@ class PostControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Nested
+    @DisplayName("list 메소드는")
+    class Describe_list{
+        @Test
+        @DisplayName("Post 전체와 200을 리턴한다.")
+        void itReturnAllPostAnd200() throws Exception {
+            mockMvc.perform(get("/post/list"))
+                    .andExpect(status().isOk());
+        }
+    }
 
 }
