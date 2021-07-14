17eef797a727649389b04222315735c0f7a58426
weno <logicalstark1105@gmail.com>
2021-07-14 12:45:34 +0900

    feat: PostClass 속성 생성하라

--- src/main/java/com/example/handleError/entity/Post.java
+++ src/main/java/com/example/handleError/entity/Post.java
@@ -1,4 +1,41 @@
 package com.example.handleError.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+    public Post() {}
+
+    public Post(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
 }
