996c29b92c8143f30550a15553313cc183ac3824
weno <logicalstark1105@gmail.com>
2021-07-14 12:20:59 +0900

    test: PostRepitory mock으로 주입받기 실패하라

--- src/test/java/com/example/handleError/service/PostServiceTest.java
+++ src/test/java/com/example/handleError/service/PostServiceTest.java
@@ -1,9 +1,15 @@
 package com.example.handleError.service;
 
+import com.example.handleError.entity.PostRepository;
 import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
 
 
 class PostServiceTest {
+
+    private PostRepository postRepository = mock(PostRepository.class);
+    private PostService postService = new PostService(postRepository);
 
     @Test
     void testList(){
