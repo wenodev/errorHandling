d968cef0e503335375227f057afe6d824b483a2f
weno <logicalstark1105@gmail.com>
2021-07-28 10:36:36 +0900

    feat: create 메소드 생성하라

--- src/main/java/com/example/handleError/controller/PostController.java
+++ src/main/java/com/example/handleError/controller/PostController.java
@@ -2,8 +2,12 @@ package com.example.handleError.controller;
 
 import com.example.handleError.dto.PostData;
 import com.example.handleError.service.PostService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,5 +25,11 @@ public class PostController {
     @GetMapping("list")
     public List<PostData> list(){
         return postService.list();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/create")
+    public List<PostData> create(@RequestBody PostData postData){
+        return null;
     }
 }
