7b44df414b94c77228c2e599cb4a0f83f7f05960
weno <logicalstark1105@gmail.com>
2021-07-28 10:25:25 +0900

    test: create 메소드 실패하는 테스트 작성하라

--- src/test/java/com/example/handleError/controller/PostControllerTest.java
+++ src/test/java/com/example/handleError/controller/PostControllerTest.java
@@ -1,6 +1,8 @@
 package com.example.handleError.controller;
 
+import com.example.handleError.dto.PostData;
 import com.example.handleError.service.PostService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -22,6 +24,8 @@ class PostControllerTest {
     @MockBean
     private PostService postService;
 
+    private ObjectMapper mapper = new ObjectMapper();
+
     @Nested
     @DisplayName("list 메소드는")
     class Describe_list{
@@ -30,6 +34,31 @@ class PostControllerTest {
         void itReturnAllPostAnd200() throws Exception {
             mockMvc.perform(get("/post/list"))
                     .andExpect(status().isOk());
+        }
+    }
+
+    @Nested
+    @DisplayName("create 메소드는")
+    class Describe_create{
+
+        private final static String createTitle = "CREATE_TITLE";
+        private final static String createContent = "CREATE_CONTENT";
+
+        PostData postData = PostData.builder()
+                .title(createTitle)
+                .content(createContent)
+                .build();
+
+        @Nested
+        @DisplayName("만약 title과 content가 존재 한다면")
+        class Context_with_existed_title_and_content{
+            @Test
+            @DisplayName("주어진 postData를 저장하고 리턴한다.")
+            void it_returns_save_postData_and_return() throws Exception {
+                mockMvc.perform(get("/post/create")
+                        .content(mapper.writeValueAsString(postData)))
+                        .andExpect(status().isCreated());
+            }
         }
     }
 
