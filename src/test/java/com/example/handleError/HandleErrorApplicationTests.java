456e275ddef9d1e3c39349ace9f08a0fb6fd833c
weno <logicalstark1105@gmail.com>
2021-07-29 15:12:34 +0900

    fix: 불필요한 코드 제거하라

--- src/test/java/com/example/handleError/HandleErrorApplicationTests.java
+++ src/test/java/com/example/handleError/HandleErrorApplicationTests.java
@@ -1,13 +1,4 @@
 package com.example.handleError;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class HandleErrorApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
