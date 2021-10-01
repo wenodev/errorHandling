201cb537f940275f63343bcf3760e769ec36011e
wenodev <logicalstark1105@gmail.com>
2021-10-01 15:01:02 +0900

    feat: create QueryDslConfiguration class

---
+++ src/main/java/com/example/handleError/config/QueryDslConfiguration.java
@@ -0,0 +1,22 @@
+package com.example.handleError.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PrePersist;
+
+@Configuration
+public class QueryDslConfiguration {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(){
+        return new JPAQueryFactory(entityManager);
+    }
+
+}
